cdf4a847093314cace5afd70f46f701e7e4ff3ae
Gregorio Martocci <gregoriomartocci@gmail.com>
2021-08-18 22:25:55 -0300

    yendo

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-var controller = require("../controllers/index");
-
-router.get("/", controller.test);
-
-module.exports = router;
